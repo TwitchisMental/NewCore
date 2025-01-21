2c9c70b8179fd54c65ba33361b4a782c13c01bf2
TwitchisMental <98340766+TwitchisMental@users.noreply.github.com>
2025-01-21 14:05:14 -0600

    Initial Upload

---
+++ languages/ThemeStrings.english.php
@@ -0,0 +1,8 @@
+<?php
+
+
+$txt['themecopyright'] = '<a href="https://www.simplemachines.org/community/index.php?action=profile;u=218416">Aurora SMF Theme Made By : TwitchisMental</a>';
+$txt['totalTimeLogged'] = 'Total time logged in: &nbsp;'
+
+
+?>
